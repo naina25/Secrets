39050e137da8f094281b46c31becce3cd580ca38
naina <nanu02d8@gmail.com>
2020-12-22 23:26:57 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(express.static("public"));
+
+app.listen(3030, function() {
+    console.log("Server started on port 3030");
+  });
+  
