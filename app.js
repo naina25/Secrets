38874b1339a596cc1523a65964a429ac9b6cda20
naina <nanu02d8@gmail.com>
2020-12-23 00:05:22 +0530

    connect database and add post route for register

--- app.js
+++ app.js
@@ -6,10 +6,44 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+    email:String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
+
 app.use(bodyParser.urlencoded({
     extended: true
   }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err) => {
+        if (err){
+            res.send(err);
+        }else {
+            res.render("secrets");
+        }
+    })
+});
 
 app.listen(3030, function() {
     console.log("Server started on port 3030");
