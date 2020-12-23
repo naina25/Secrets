7ae0dd8a117bd27d69b5d53cef6b78e216bc2c08
naina <nanu02d8@gmail.com>
2020-12-23 13:10:04 +0530

    Add encryption using secret key

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -9,10 +10,13 @@ app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email:String,
     password: String
-}
+});
+
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt, {secret:secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
